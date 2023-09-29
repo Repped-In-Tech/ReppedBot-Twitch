4d4a131ef6bc37672237ffd856098a689104e922
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-29 13:31:57 -0500

    added ability to add task:

--- server.js
+++ server.js
@@ -18,7 +18,46 @@ const getUserTasks = async (username) => {
   return data;
 }
 
+const updateTask = (firebaseKey, payload) => {
+  return fetch(`${process.env.FIREBASE_DB_URL}/${firebaseKey}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload),
+    }
+  ).then(response => response.json());
+}
 
+const addTask = (username, task) => {
+  const payload = {
+    username,
+    task,
+    isDone: false,
+    createAt: Date.now(),
+    completedAt: null,
+    firebaseKey: null
+  }
+  try {
+    return fetch(`${process.env.FIREBASE_DB_URL}.json`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+      }
+    )
+    .then((response) => response.json())
+    .then(data => {
+      const firebaseKey = data.name;
+      return updateTask(firebaseKey, { firebaseKey }).then(obj => obj);
+    })
+  }
+  catch (error) {
+    console.log(error)
+  }
+  
+}
 
 
 client.on('message', (channel, tags, message, self) => {
@@ -28,6 +67,7 @@ client.on('message', (channel, tags, message, self) => {
   const args = message.slice(1).split(' ');
 	const command = args.shift().toLowerCase();
 
+  // TODO: convert to switch
 	if(command === 'viewtasks') {
     getUserTasks(tags.username).then((data) => {
       // filter the tasks for the incomplete ones
@@ -35,12 +75,24 @@ client.on('message', (channel, tags, message, self) => {
       // check if there are any incomplete tasks
       if (notComplete.length) {
         const taskList = notComplete.map((item, index) => `${index +1}: ${item.task}`)
-  
         client.say(channel, `@${tags.username}, You have a total of ${taskList.length} incomplete tasks: ${taskList.join(", ")}`);
       } else {
         client.say(channel, `@${tags.username}, You do not have any incomplete tasks! To add a task, enter !addTask YOUR TASK`);
       }
     });
 	}
+
+  if(command === 'addtask') {
+    // create a task!
+    addTask(tags.username, args.join(" ")).then(() => {
+      client.say(channel, `@${tags.username}, your task was added!`);
+    })
+  }
+  if(command === 'deletetask') {
+    // delete a task!
+  }
+  if(command === 'edittask') {
+    // edit a task!
+  }
 });
 

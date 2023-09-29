6ec90fb5df22f9fc379c166d5873f911df6227f4
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-29 10:01:06 -0500

    tested logic to post via bot

--- server.js
+++ server.js
@@ -12,11 +12,18 @@ const client = new tmi.Client({
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	if(self) return;
+const calcAge = (yearBorn) => {
+  return 2023 - yearBorn;
+}
 
-	if(message.toLowerCase() === '!hello') {
-		// "@alca, heya!"
-		client.say(channel, `@${tags.username}, wut up fool??!`);
+client.on('message', (channel, tags, message, self) => {
+	if(self || !message.startsWith('!')) return;
+
+  // This command handler example will split a message like this example chat message: !Echo Chat message here into the command "echo" with the arguments [ "Chat", "message", "here" ].
+  const args = message.slice(1).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if(command === 'age') {
+		client.say(channel, `@${tags.username}, you are ${calcAge(Number(args.join(' ')))} years old! `);
 	}
 });

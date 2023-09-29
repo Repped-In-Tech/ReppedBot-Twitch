e988e20a7ade4c16cbd660427710177f163a5861
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-29 14:26:53 -0500

    updated readme and added done logic

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # reppedbot-twitch
 
+Twitch chat bot that uses Firebase to keep track of user tasks.
+
+## Commands
+- !addtask - Adds a task
+- !donetask [tasknum] - marks a task as complete
+- !edittask [tasknum] [updated todo] - Edits a task
+- !deletetask [tasknum] - Deletes a task
+- !viewtasks - View all YOUR tasks
+
+
 ## Dependencies
+- Firebase
 - https://tmijs.com/
 - https://www.npmjs.com/package/dotenv
--- server.js
+++ server.js
@@ -162,4 +162,32 @@ client.on("message", (channel, tags, message, self) => {
       }
     });
   }
+  if (command === "donetask") {
+    getUserTasks(tags.username).then((data) => {
+      // filter the tasks for the incomplete ones
+      const notComplete = Object.values(data).filter((task) => !task.isDone);
+      // check if there are any incomplete tasks
+      if (
+        notComplete.length &&
+        Number(args.join("")) &&
+        Number(args.join("")) <= notComplete.length
+      ) {
+        updateTask(notComplete[Number(args.join("")) - 1].firebaseKey, {isDone: true}).then(() => {
+          client.say(
+            channel,
+            `@${
+              tags.username
+            }, Your task has been marked as complete! You now have a total of ${
+              notComplete.length - 1
+            } incomplete tasks. To view your tasks, use !viewTasks`
+          );
+        });
+      } else {
+        client.say(
+          channel,
+          `@${tags.username}, You do not have any incomplete tasks to mark as done OR you entered an incorrect value! To mark a task as done, enter !doneTask [task num]`
+        );
+      }
+    });
+  }
 });

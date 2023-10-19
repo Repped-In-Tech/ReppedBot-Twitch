12d4599b2416ef2641d2aa86f4a72394a6a21572
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-10-19 14:15:51 -0500

    added app router based on command

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const { hustleBot } = require("./apps/hustleList/bot.js");
-const { client } = require("./client.js");
+const { hustleBot } = require('./apps/hustleList/bot.js');
+const { client } = require('./client.js');
 
 const appCmdLists = {
-  hustle: ['viewtasks', 'viewtask', 'addtask', 'edittask', 'deletetask', 'donetask', 'viewducks', 'viewduck','rubberduck'],
-  ai: ['reppedbot'],
-  mutherDucker: [],
-  pomodoro: ['!pom'],
+  viewtasks: 'hustle',
+  viewtask: 'hustle',
+  addtask: 'hustle',
+  edittask: 'hustle',
+  deletetask: 'hustle',
+  donetask: 'hustle',
+  viewducks: 'hustle',
+  viewduck: 'hustle',
+  rubberduck: 'hustle',
+  reppedbot: 'ai',
+  // mutherDucker: [],
+  // pomodoro: ['!pom'],
 };
 
 client.connect();
 
-client.on("message", (channel, tags, message, self) => {
-  if (self || !message.startsWith("!")) return;
+client.on('message', (channel, tags, message, self) => {
+  if (self || !message.startsWith('!')) return;
 
   // This command handler example will split a message like this example chat message: !Echo Chat message here into the command "echo" with the arguments [ "Chat", "message", "here" ].
-  const args = message.slice(1).split(" ");
+  const args = message.slice(1).split(' ');
   const command = args.shift().toLowerCase();
-  const combinedArgs = {channel, tags, args, command};
-  // TODO: 1. check which list of commands the command belongs to so that we know which app to run
-
-  // TODO: 2. Call the app and send it all the args so that the app can run
-  hustleBot(combinedArgs);
+  const combinedArgs = { channel, tags, args, command };
+  // check which list of commands the command belongs to so that we know which app to run
+  switch(appCmdLists[command]) {
+    //call the app and send it all the args so that the app can run
+    case 'hustle':
+      hustleBot(combinedArgs);
+      break;
+  }
 });

5b3ce56e22a2e380850a36232bdf9d8036c38887
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-11-02 12:55:53 -0500

    added twork status commands

--- apps/reppedbot/bot.js
+++ apps/reppedbot/bot.js
@@ -4,51 +4,49 @@ const { addToSquad, getSquad, checkIfSquaded, updateSquadMember, deleteFromSquad
 const reppedBot = ({ channel, tags, args, command }) => {
   switch (command) {
     case 'lurk':
+    case 'twork':
       checkIfSquaded(tags.username).then((data) => {
         const squaded = Object.values(data).length;
 
         if (squaded) {
           const { reppedSquad, firebaseKey } = Object.values(data)[0];
           // if you are on lurksquad already, say so
-          if (reppedSquad === 'lurksquad') {
-            say(channel, `@${tags.username}, you already repping the lurk squad. Don't trip! You can't double dip!`);
+          if (reppedSquad === `${command}squad`) {
+            say(channel, `@${tags.username}, you already repping the ${command} squad. Don't trip! You can't double dip!`);
           } else {
-            const payload = { reppedSquad: 'lurksquad' };
-            updateSquadMember(firebaseKey, payload).then(() => say(channel, `@${tags.username}, You have been switched to the lurk squad ✊🏾! Make sure you are LURKING and 💃🏻 TWORKING 🕺🏻`));
+            const payload = { reppedSquad: `${command}squad` };
+            updateSquadMember(firebaseKey, payload).then(() => say(channel, `@${tags.username}, You have been switched to the ${command} squad ✊🏾!`));
           }
           // if you are not on lurksquad add to lurk squad and say so
         } else {
-          addToSquad(tags.username, 'lurksquad').then(() => say(channel, `@${tags.username}, thanks for joining the lurk squad ✊🏾! Make sure you are LURKING and 💃🏻 TWORKING 🕺🏻`));
+          addToSquad(tags.username, `${command}squad`).then(() => say(channel, `@${tags.username}, thanks for joining the ${command} squad ✊🏾!`));
         }
       });
       break;
     case 'unlurk':
+    case 'untwork':
       checkIfSquaded(tags.username).then((data) => {
         const squaded = Object.values(data).length;
-
+        const [, splitCmd] = command.split('un');
         if (squaded) {
           const { reppedSquad, firebaseKey } = Object.values(data)[0];
-          // if you are on lurksquad already, say so
-          if (reppedSquad === 'lurksquad') {
-            deleteFromSquad(firebaseKey)
-              .then(() => say(channel, `@${tags.username}, you have been removed from the lurk squad`))
+          if (reppedSquad === `${splitCmd}squad`) {
+            deleteFromSquad(firebaseKey).then(() => say(channel, `@${tags.username}, you have been removed from the ${splitCmd} squad`));
           } else {
-            say(channel, `@${tags.username}, you ain't even on the lurk squad...what you tryna do???`);
+            say(channel, `@${tags.username}, you ain't even on the ${splitCmd} squad...what you tryna do???`);
           }
           // if you are not on lurksquad add to lurk squad and say so
         } else {
-          say(channel, `@${tags.username}, you ain't even on the lurk squad...what you tryna do???`);
+          say(channel, `@${tags.username}, you ain't even on the ${splitCmd} squad...what you tryna do???`);
         }
       });
       break;
     case 'lurksquad':
-      getSquad('lurksquad').then((squadObject) => {
+    case 'tworksquad':
+      getSquad(`${command}`).then((squadObject) => {
         const squadMembers = Object.values(squadObject).map((item) => `@${item.username}`);
-        squadMembers.length ? say(channel, `🫣 LURK SQUAD: ${squadMembers.length >= 1 ? squadMembers.join(', ') : squadMembers[0]}`) : say(channel, `No one is LURKING...we all TWORKING!`);
+        squadMembers.length ? say(channel, `${command === 'lurksquad' ? '🫣 LURK SQUAD' : '💃🏻🕺🏻 TWORK SQUAD'}: ${squadMembers.length >= 1 ? squadMembers.join(', ') : squadMembers[0]}`) : say(channel, `No one is ${command === 'lurksquad' ? 'LURKING' : 'TWORKING'}!`);
       });
-      break;
-    case 'twork':
-      say(channel, `@${tags.username}, Oh...so you want err body to know you likes to twork?! Alright, then! Getcho TWORK on den! 💃🏻🕺🏻💃🏻🕺🏻💃🏻🕺🏻💃🏻🕺🏻`);
       break;
   }
 };
